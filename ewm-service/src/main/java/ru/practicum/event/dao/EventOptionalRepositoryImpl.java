0d3f7c8065e79150d25e52acc8985f03315bfd08
unknown <SVyakimchuk@yandex.ru>
2023-08-23 21:40:26 +0400

    style: исправить замечания по checkstyle

--- ewm-service/src/main/java/ru/practicum/event/dao/EventOptionalRepositoryImpl.java
+++ ewm-service/src/main/java/ru/practicum/event/dao/EventOptionalRepositoryImpl.java
@@ -20,7 +20,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
-public class EventOptionalRepositoryImpl implements EventOptionalRepository{
+public class EventOptionalRepositoryImpl implements EventOptionalRepository {
 
     private final EntityManager entityManager;
     private final CriteriaBuilder cb;
